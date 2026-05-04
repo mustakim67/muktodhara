15fd2436cffd5b607637099a3ec12209483be6d4
mustakim67 <mahfujapple95@gmail.com>
2026-05-05 01:09:35 +0600

    adhn

--- app/api/analytics/route.js
+++ app/api/analytics/route.js
@@ -1,5 +1,7 @@
-// // Example Aggregation for 30-day "RED" count
-// const vulnerability = await Sensor.aggregate([
-//   { $match: { createdAt: { $gte: new Date(Date.now() - 30*24*60*60*1000) } } },
-//   { $group: { _id: "$status", count: { $sum: 1 } } }
-// ]);
+// Example Aggregation for 30-day "RED" count
+export const dynamic = 'force-dynamic';
+
+const vulnerability = await Sensor.aggregate([
+  { $match: { createdAt: { $gte: new Date(Date.now() - 30*24*60*60*1000) } } },
+  { $group: { _id: "$status", count: { $sum: 1 } } }
+]);
